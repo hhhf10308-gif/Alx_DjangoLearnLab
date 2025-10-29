c84b44dc606996735e8589235566b097ae38b5b3
Ahmed <hhhf10308@gmail.com>
2025-10-29 19:11:41 +0300

    Delete django_blog/blog directory

--- django_blog/blog/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Post
-from django.contrib.auth.models import User
-
-class PostSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Post
-        fields = '__all__'
